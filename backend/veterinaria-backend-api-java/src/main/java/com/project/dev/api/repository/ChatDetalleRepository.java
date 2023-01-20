d91ba5becb430abd541ea394180e376eb62c3f8e
Dyson <dysontilano@gmail.com>
2023-01-20 18:17:34 -0500

    Removed unused Repository method

--- backend/veterinaria-backend-api-java/src/main/java/com/project/dev/api/repository/ChatDetalleRepository.java
+++ backend/veterinaria-backend-api-java/src/main/java/com/project/dev/api/repository/ChatDetalleRepository.java
@@ -32,14 +32,6 @@ import org.springframework.data.repository.query.Param;
 public interface ChatDetalleRepository extends JpaRepository<ChatDetalle, Long> {
 
     /**
-     * Obtiene los registros de la base de datos con un id indicado.
-     *
-     * @param id es el id a buscar en base de datos.
-     * @return los registros obtenidos.
-     */
-    public List<ChatDetalle> findByIntIdChatDetalle(Long id);
-
-    /**
      * Busca en base de datos los registros que contengan una cadena específica.
      *
      * @param strSearch la cadena a buscar en base de datos.
--- backend/veterinaria-backend-api-java/src/main/java/com/project/dev/api/repository/PersonaRepository.java
+++ backend/veterinaria-backend-api-java/src/main/java/com/project/dev/api/repository/PersonaRepository.java
@@ -32,14 +32,6 @@ import org.springframework.data.repository.query.Param;
 public interface PersonaRepository extends JpaRepository<Persona, Long> {
 
     /**
-     * Obtiene los registros de la base de datos con un id indicado.
-     *
-     * @param id es el id a buscar en base de datos.
-     * @return los registros obtenidos.
-     */
-    public List<Persona> findByIntIdPersona(Long id);
-
-    /**
      * Busca en base de datos los registros que contengan una cadena específica.
      *
      * @param strSearch la cadena a buscar en base de datos.
