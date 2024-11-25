a217f55a39c29b5758771b23cababa1c39f1b7a9
unknown <dysontilano@gmail.com>
2024-11-24 23:07:23 -0500

    Documentation updated and source code refactorized

--- backend/veterinaria-backend-api-java/src/main/java/com/project/dev/api/service/mapping/FotoMapping.java
+++ backend/veterinaria-backend-api-java/src/main/java/com/project/dev/api/service/mapping/FotoMapping.java
@@ -29,23 +29,21 @@ import org.mapstruct.Mapping;
 public interface FotoMapping extends GenericMapping<FotoDTO, Foto> {
 
     /**
-     * Obtiene una entidad en base a su DTO.
+     * Obtiene una entidad en base a su DTO usando el campo clave de la base de datos.
      *
      * @param dto es el DTO a convertir en entidad.
      * @return la entidad equivalente al dto.
      */
-    // Deben ser el campo clave de la base de datos.
     @Mapping(source = "intIdFoto", target = "intIdFoto")
     @Override
     public Foto getEntity(FotoDTO dto);
 
     /**
-     * Obtiene un DTO en base a su entidad.
+     * Obtiene un DTO en base a su entidad usando el campo clave de la base de datos.
      *
      * @param entity es la entidad a convertir en DTO.
      * @return el dto equivalente a la entidad.
      */
-    // Deben ser el campo clave de la base de datos.
     @Mapping(source = "intIdFoto", target = "intIdFoto")
     @Override
     public FotoDTO getDto(Foto entity);
